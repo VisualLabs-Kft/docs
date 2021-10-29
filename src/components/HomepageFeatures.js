b28b507594d1d8c156683d5813ad865ab0b295ae
VISUALLABSKFT <60983606+balazs-z-horvath@users.noreply.github.com>
2021-10-29 22:34:45 +0200

    fooldal és színek

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Fun dolgok',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Céges mókákról emlékek, beszámolók.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Tudásbázis',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Elérhetőek a termékek leírásai, a best practise-eink, a vizsga-felkészítő anyagok
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Biblia',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        A csapat működésének alapvetései.
       </>
     ),
   },
